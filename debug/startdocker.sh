88f8153853b7de20f5d906328be3d802fa9a6a2f
Aleksej v <achmed21@gmail.com>
2016-03-27 16:39:09 +0200

    changes

--- debug/startdocker.sh
+++ debug/startdocker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-docker rm -f dc-mysql
+docker rm -f mysql
 docker run -d \
 -p 3307:3306 \
 -e MYSQL_USER=admin \
 -e MYSQL_PASS=admin \
 -e ON_CREATE_DB=dcold \
--e STARTUP_SQL="/data/debug/sql/import.sql" \
--v /home/achmed/dcplus2phpbb:/data \
 --restart=always \
---name="dc-mysql" \
+--name="mysql" \
 tutum/mysql
